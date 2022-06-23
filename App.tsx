06aac80a241292f83d49aad976718c36164ebb6d
Matt Jewell <m@mattjewell.co.uk>
2022-06-23 22:07:05 +0200

    added tailwind

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import tw from "tailwind-rn";
 
+// noinspection JSUnusedGlobalSymbols
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text style={tw("text-blue-600")}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
