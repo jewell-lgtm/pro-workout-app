652f324a59c08474be821511557bbedd3cb06651
Matt Jewell <m@mattjewell.co.uk>
2022-07-28 13:07:06 +0200

    hoisting limit to workspaces

--- apps/rn/metro.config.js
+++ apps/rn/metro.config.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
 const { createMetroConfiguration } = require("expo-yarn-workspaces")
 
+// eslint-disable-next-line
 module.exports = createMetroConfiguration(__dirname)
